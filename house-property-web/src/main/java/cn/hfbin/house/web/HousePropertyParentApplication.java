b73729934d84e8c286e4c1d28d94937a57eedc09
hfbin <1716850375@qq.com>
2018-09-09 20:55:23 +0800

    house-property-web

---
+++ house-property-web/src/main/java/cn/hfbin/house/web/HousePropertyParentApplication.java
@@ -0,0 +1,12 @@
+package cn.hfbin.house.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousePropertyParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousePropertyParentApplication.class, args);
+	}
+}
