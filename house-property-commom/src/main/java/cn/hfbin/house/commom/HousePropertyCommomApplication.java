e211e38147739f2884a30d7d073abc68e85e496f
hfbin <1716850375@qq.com>
2018-09-09 21:00:57 +0800

    house property commom

---
+++ house-property-commom/src/main/java/cn/hfbin/house/commom/HousePropertyCommomApplication.java
@@ -0,0 +1,12 @@
+package cn.hfbin.house.commom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousePropertyCommomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousePropertyCommomApplication.class, args);
+	}
+}
